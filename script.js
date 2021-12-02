138a79416b714e8f6c9aa3508ae990fad6403c8f
Kirill M <94078612+FuturisticZigZag@users.noreply.github.com>
2021-12-02 21:00:39 +0100

    Added the right files this time...

--- script.js
+++ script.js
@@ -1,44 +1,101 @@
-//========Skapar konstanter======
-const userInput = document.getElementById("userInput");
-const btn = document.getElementById("btn");
-const userOutput = document.getElementById("output");
+//======LOCAL STORAGE UPPGIFTER=======
+//placeholder uppgifter
+const username = "aaa";
+const password = "123";
+
+//============
+//EJ INLOGGAD:
+//============ 
+
+//Skapa logIN fält i header
+function notLoggedIn() {
+    //===Användar namn fält
+    let usernameInputField = document.createElement("input");
+        usernameInputField.placeholder = "Användarnamn";
+
+    //===Lösenord fält
+    let passwordInputField = document.createElement("input");
+        passwordInputField.placeholder = "Lösenord";
+        passwordInputField.type = "password";
+
+    //===Login knapp
+    let loginButton = document.createElement("button");
+        loginButton.innerText = "Logga In";
+    document.getElementById("logInlogOut").append(usernameInputField, passwordInputField, loginButton);
+
+    //Fånga upp användar uppgifter med click
+    loginButton.addEventListener("click", () => {
+        let usernameInput = usernameInputField.value
+        let passwordInput = passwordInputField.value
+
+        //Om rätt uppgifter byt vy till INLOGGAD
+        if(usernameInput == username && passwordInput == password) {
+            usernameInputField.remove();
+            passwordInputField.remove();
+            loginButton.remove();
+            LoggedIn();
+
+        //Om fel uppgifter byt vy till FEL INLOGGNING    
+        } else {
+            wrongLogin();
+        }
+    })
+};
+notLoggedIn();
 
 
-//========Fångar upp user input in i local storage====
-btn.addEventListener("click", () => {
-    const userInputValue = userInput.value;
+//Main - Uppmana användaren att logga in, registrerings fält
+ 
 
-    //======Kollar om man har skrivit något i fältet===
-    if (userInputValue) {
-        localStorage.setItem("userText", userInputValue);
-        
-        //=====Laddar om sidan======
-        location.reload();
+        //Fånga upp användar uppgifter med click
 
-    //=====Uppmanar användaren att skriva något i fältet====    
-    } else {
-        document.getElementById("output").innerHTML = "";
-        document.getElementById("output").append("Vänligen skriv något!");
-    }
-})
+            //Hämta local storage uppgifter
 
-//=======Printar user input från local storage=====
-for (i=0; i < localStorage.length; i++) {
-    let userText = localStorage.getItem("userText");
-    document.getElementById("output").append(userText);
+            //Ändra uppgifterna - lägg till ny användare
 
-    //======Skapar en reset knapp=====
-    let resetBtn = document.createElement("button");
-        resetBtn.innerText = "Ta bort!";
-    let lineBreak = document.createElement("br");
-    document.getElementById("output").append(lineBreak, resetBtn);
+            //Spara uppgifterna
 
-    //======Gör så att reset knappen tömmer local storage
-    resetBtn.addEventListener("click", () => {
-        localStorage.clear();
+            //Bekräfta att användare är registrerad och kan logga in nu
 
-        //======Laddar om sidan=====
+ 
+
+ //=========
+ //INLOGGAD:
+ //=========
+ //Header - visa logOUT knapp och en logga(bild eller text)   
+
+//Skapa logOUT knapp i header
+function LoggedIn(){
+
+    //===Login knapp
+    let logOutButton = document.createElement("button");
+    logOutButton.innerText = "Logga Ut";
+    document.getElementById("logInlogOut").append(logOutButton);
+
+    //Main - Välkomna "Username"
+    document.getElementById("main").innerHTML = "Välkommen 'username'!"
+
+    //Vid click byt vy till EJ INLOGGAD
+    logOutButton.addEventListener("click", () => {     
+        logOutButton.remove();  
+        notLoggedIn();
         location.reload();
     })
-
 };
+
+ 
+
+//===============
+//FEL INLOGGNING:
+//===============
+//Main - Meddela användaren att uppgifetrna är fel
+function wrongLogin() {
+    let headingText = document.getElementById("headingText");
+        headingText.innerText = "Oj! Det verkar som att du uppgave fel uppgifter!"
+        document.getElementById("headingText").style.color = "red";
+
+    let callToAction = document.getElementById("callToAction");
+        callToAction.innerText = "Vänligen testa att logga in igen eller registrera dig nedan om du är en ny användare!"    
+        document.getElementById("callToAction").style.color = "red";
+};
+
