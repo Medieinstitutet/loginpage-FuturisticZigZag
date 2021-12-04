71127fd7402bbc569cd9d2ae2abef1d415f59cc1
Kirill M <94078612+FuturisticZigZag@users.noreply.github.com>
2021-12-04 16:22:17 +0100

    +Users stored in LS +Documentation -Bugs

--- script.js
+++ script.js
@@ -3,18 +3,40 @@ const loginStatus = localStorage.getItem("loginStatus");
 
 const storedUsers = [
     {
-    "username": "aaa",
-    "password": "123"
+    username: "janne",
+    password: "test"
     },
     {
-    "username": "bbb",
-    "password": "12345"
+    username: "aaa",
+    password: "123"
     },
     {
-    "username": "bbb",
-    "password": "666"
+    username: "bbb",
+    password: "666"
     }
 ];
+
+
+//Skapar en första local storage med testanvändare från array ovan, 
+//förhindrar att LS skrivs över med testanvändare vid varje omladdning
+function firstLS() {
+    let checkLS = localStorage.getItem("storedUsers");
+
+    if(checkLS) {
+        return;
+    } else {
+        localStorage.setItem("storedUsers", JSON.stringify(storedUsers));
+        return;
+    }
+};
+
+firstLS();
+
+//let getStoredUsersLS = JSON.parse(localStorage.getItem("storedUsers"))
+
+function saveStoredUsersLS() {
+     localStorage.setItem("storedUsers", JSON.stringify(getStoredUsersLS));
+};
 
 //============
 //EJ INLOGGAD:
@@ -44,12 +66,14 @@ function notLoggedIn() {
         //Ändrar status på inlogningen så att rätt vy kan visas
         localStorage.setItem("loginStatus", "yes");
 
-        //Om rätt uppgifter byt vy till INLOGGAD
-        let currentUser = storedUsers.filter(function(user) {
+        //Kollar så att user input matchar med användare i array
+        let getStoredUsersLS = JSON.parse(localStorage.getItem("storedUsers"))
+
+        let currentUser = getStoredUsersLS.filter(function(user) {
             return user.username == usernameInput && user.password == passwordInput;
         })
 
-
+        //"Döljer" logginfältet
         if(currentUser.length) {
             usernameInputField.remove();
             passwordInputField.remove();
@@ -66,47 +90,64 @@ function notLoggedIn() {
     })
 };
 
-//Visar rätt vy beroende på inlognings status (Viktigast att logged in vyn inte försvinner efter en refresh)
-if (loginStatus == "no"){
-    notLoggedIn();
-} else {
+//Visar rätt vy beroende på inlognings status 
+if (loginStatus == "yes"){
     LoggedIn();
+} else {
+    notLoggedIn();
 }
 
 
-    
+//========
+//REG FÄLT
+//========   
 
-//Main - Uppmana användaren att logga in, registrerings fält
- 
-
-//Fånga upp användar uppgifter med click
 let userNameReg = document.getElementById("userNameReg");
 let userPasswordReg = document.getElementById("userPasswordReg");
 let regBtn = document.getElementById("regBtn");
 
+//Fånga upp användar uppgifter med click
 regBtn.addEventListener("click", () => {
+    //Sparar uppgifterna i variabler en för att kolla om fälten är toma
+    let userInputValueName = document.getElementById("userNameReg").value;
+    let userInputValuePass = document.getElementById("userPasswordReg").value;
+
+    //Sparar uppgifterna i en variabel som ska in i local storage sen
     let userInputValue = {
         "username": document.getElementById("userNameReg").value,
         "password": document.getElementById("userPasswordReg").value
     };
-    storedUsers.push(userInputValue);
-    console.log(storedUsers);
+    
+    if((userInputValueName == "" && userInputValuePass == "") || userInputValueName == "" || userInputValuePass == "") {
+        
+        //Kolla om fälten är toma
+        let callToAction = document.getElementById("callToAction");
+        callToAction.innerText = "Vänligen välj ett Anvädarnamn och Lösenord!"    
+        document.getElementById("callToAction").style.color = "red";
+
+    } else {
+        //Bekräftar registreringen
+        let callToAction = document.getElementById("callToAction");
+        callToAction.innerText = "Du är nu registrerad!"    
+        document.getElementById("callToAction").style.color = "green";
+
+        //Hämtar local storage uppgifter
+        getStoredUsersLS = JSON.parse(localStorage.getItem("storedUsers"))
+
+        //Pushar variabeln med uppgifterna in i LS array
+        getStoredUsersLS.push(userInputValue);
+
+        console.log(getStoredUsersLS);
+
+        //Sparar nya array i LS
+        saveStoredUsersLS();
+        }
+
 })
-
-            //Hämta local storage uppgifter
-
-            //Ändra uppgifterna - lägg till ny användare
-
-            //Spara uppgifterna
-
-            //Bekräfta att användare är registrerad och kan logga in nu
-
- 
 
  //=========
  //INLOGGAD:
  //=========
- //Header - visa logOUT knapp och en logga(bild eller text)   
 
 //Skapa logOUT knapp i header
 function LoggedIn(){
@@ -127,6 +168,8 @@ function LoggedIn(){
         
         logOutButton.remove();  
         notLoggedIn();
+
+        //Laddar om sidan för att "reset'a main innerHTML"
         location.reload();
     })
 };
@@ -139,7 +182,7 @@ function LoggedIn(){
 //Main - Meddela användaren att uppgifetrna är fel
 function wrongLogin() {
     let headingText = document.getElementById("headingText");
-        headingText.innerText = "Oj! Det verkar som att du uppgave fel uppgifter!"
+        headingText.innerText = "Oj! Det verkar som att du uppgav fel uppgifter!"
         document.getElementById("headingText").style.color = "red";
 
     let callToAction = document.getElementById("callToAction");
